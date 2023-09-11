987409a1245e1f9502bd729e4ac02f1db760aad0
Michael Alan Stowe <mastowe94@gmail.com>
2023-09-11 17:31:51 -0500

    complete lab

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,21 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Movies Page</h1>
+    {movies.map(movie => {
+      return (
+        <div key={movie.title}>
+          <h3>Title: {movie.title}</h3>
+          <h4>Time: {movie.time}</h4>
+          <ul>Genres: 
+            {movie.genres.map(genre =>{
+            return <li key={genre}>{genre}</li>
+          })}</ul>
+        </div>
+      )
+    })}
+  </div>;
 }
 
 export default Movies;
